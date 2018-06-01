dfbb506524cde67fb2acb39c43888350db11f9bd
Xiya Yang <xyang@magento.com>
2018-06-01 12:16:01 -0500

    magento-engcom/community-portal#1 Add TypeScript support to the project
    - Added @types/express as a development dependency
    - Changed tsconfig.json to enforce stricter checks
    - Changed .ts files to comply

--- functions/handlers/projects/handler.ts
+++ functions/handlers/projects/handler.ts
@@ -1,11 +1,12 @@
 'use strict';
 
+import express = require('express');
+
 const serverless = require('serverless-http');
-const express = require('express')
 const project = require('./../../src/projects/projects');
 const app = express()
 
-app.get('/projects/', function (req, res) {
+app.get('/projects/', function (req:express.Request, res:express.Response) {
   var projectsService = new project();
   res.send(projectsService.getProjects())
 })
--- functions/handlers/users/handler.ts
+++ functions/handlers/users/handler.ts
@@ -1,11 +1,12 @@
 'use strict';
 
+import express = require('express');
+
 const serverless = require('serverless-http');
-const express = require('express');
 const users = require('./../../src/users/users');
 const app = express()
 
-app.get('/users/', function (req, res) {
+app.get('/users/', function (req:express.Request, res:express.Response) {
   var usersService = new users();
   res.send(usersService.getUsers(req))
 })
