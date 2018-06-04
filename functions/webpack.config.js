69eed8772e6b079ec47039a89c58197af211e5b5
Xiya Yang <xyang@magento.com>
2018-06-04 14:51:08 -0500

    magento-engcom/community-portal#1 Add TypeScript support to the project
    - Removed /frontend/bundle.js
    - Removed commented out code in /functions/webpack.config.js
    - Removed source-map-support from /functions/package.json

--- functions/webpack.config.js
+++ functions/webpack.config.js
@@ -1,14 +1,7 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
 
-// const entries = {};
-
-// Object.keys(slsw.lib.entries).forEach(key => (
-//   entries[key] = ['./source-map-install.js', slsw.lib.entries[key]]
-// ));
-
 module.exports = {
-  // entry: entries,
   devtool: 'source-map',
   resolve: {
     extensions: [
