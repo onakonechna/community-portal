10360134fb65ef0fe6ec9c76e7510a7502be6e95
athensbird <popularwatson@gmail.com>
2018-06-07 17:26:51 -0500

    added mock ts frontend setup

---
+++ test-frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import './App.css';
+import Demo from './components/demo';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <Demo countBy={3} />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
