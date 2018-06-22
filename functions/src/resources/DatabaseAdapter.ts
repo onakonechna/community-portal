d2d3dd5424b0a76fcd0bcdd45f573e594313d578
Xiya Yang <xyang@magento.com>
2018-06-22 15:24:16 -0500

    magento-engcom/community-portal#63 Refactor the code migrated from /functions/Models in /functions/src
    - add DatabaseAdapter class to handle database operations

--- functions/src/resources/DatabaseAdapter.ts
+++ functions/src/resources/DatabaseAdapter.ts
@@ -1,18 +1,90 @@
 import { DynamoDB } from 'aws-sdk';
 
+import * as _ from 'lodash';
 import AdapterInterface from './AdapterInterface';
 
-export default class DatabaseConnection {
+export default class DatabaseAdapter implements AdapterInterface {
   private client: DynamoDB.DocumentClient;
 
-  connect() {
-    if (IS_OFFLINE === 'true') {
-      return new DynamoDB.DocumentClient({
-        region: 'localhost',
-        endpoint: 'http://localhost:8000',
-      });
-    } else {
-      return new DynamoDB.DocumentClient();
+  constructor(db: DatabaseConnection) {
+    this.db = db.connect();
+  }
+
+  create(tableName: string, data: any): Promise<any> {
+    const params = {
+      TableName: tableName,
+      Item: data,
+    };
+    return this.db.put(params).promise();
+  }
+
+  get(
+    tableName: string,
+    indexName: string,
+    expression: string,
+    nameMap: any = undefined,
+    valueMap: any = undefined,
+    ascending: boolean = true,
+  ): Promise<any> {
+
+    const params = {
+      TableName: tableName,
+      IndexName: indexName,
+      KeyConditionExpression: expression,
+      ExpressionAttributeNames: nameMap,
+      ExpressionAttributeValues: valueMap,
+      ScanIndexForward: ascending
+    };
+    return this.db.query(params).promise();
+
+  }
+
+  getById(tableName: string, identifier: any): Promise<any> {
+    const params = {
+      TableName: tableName,
+      Key: identifier,
+    };
+    return this.db.get(params).promise();
+  }
+
+  update(tableName: string, identifier: any, data: any): Promise<any> {
+    let AttributeUpdates: any = {};
+
+    _.forOwn(data, (value: any, key: string) => {
+      AttributeUpdates[key] = {
+        Action: 'PUT',
+        Value: value
+      }
+    });
+
+    const params = {
+      AttributeUpdates,
+      TableName: tableName,
+      Key: identifier,
+    };
+    return this.db.update(params).promise();
+  }
+
+  add(tableName: string, identifier: any, field: string, increment: number): Promise<any> {
+    let AttributeUpdates = {};
+    AttributeUpdates[field] = {
+      Action: 'ADD',
+      Value: increment,
     }
+
+    const params = {
+      AttributeUpdates,
+      TableName: tableName,
+      Key: identifier,
+    };
+    return this.db.update(params).promise();
+  }
+
+  delete(tableName: string, identifier: any): Promise<any> {
+    const params = {
+      TableName: PROJECTS_TABLE,
+      Key: identifier,
+    };
+    return this.db.delete(params).promise();
   }
 }
