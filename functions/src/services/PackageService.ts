53eec196159c9ac7d0df1b9bd86369107a789310
Xiya Yang <xyang@magento.com>
2018-06-25 09:36:26 -0500

    magento-engcom/community-portal#63 Refactor the code migrated from /functions/Models in /functions/src
    - add PackageService class to integrate endpoint, controllers and resources together (work in progress)

--- functions/src/services/PackageService.ts
+++ functions/src/services/PackageService.ts
@@ -14,7 +14,7 @@ import * as path from 'path';
 
 import DatabaseConnection from './../resources/DatabaseConnection';
 
-interface DataFlow {
+interface DataFlowDefinition {
   validationMap: any;
   controller: string;
   method: string;
@@ -23,9 +23,16 @@ interface DataFlow {
   targetType: string; // resource or webEndpoint
 }
 
+interface DataFlow {
+  controller: any;
+  controllerMethod: string;
+  target: any; // resource (expand to include Github APIs in the future)
+  targetMethod: string; // resource or webEndpoint
+}
+
 import Endpoint from './../Endpoint';
 
-export default class ComponentIntegration {
+export default class PackageService {
   private controllerMap: any;
   private controllerMapRoot: string;
   private resourceMap: any;
@@ -43,6 +50,10 @@ export default class ComponentIntegration {
     this.dataStore = {};
   }
 
+  createEndpoint(endpoint: Endpoint) {
+    this.endpoint = endpoint;
+  }
+
   // set the relatve path to the config files
   // file paths to the objects in the maps will be resolved relative to the root
   setControllerMapPath(configPath: string) {
@@ -57,16 +68,33 @@ export default class ComponentIntegration {
     this.webEndpointMapRoot = configPath;
   }
 
-  addDataFlow(dataFlow: DataFlow) {
-    const controller = this.createController(dataFlow.controller, dataFlow.validationMap);
+  addDataFlow(dataFlowDefinition: DataFlowDefinition) {
+    const controller = this.createController(dataFlowDefinition.controller, dataFlowDefinition.validationMap);
     let target: any;
-    if (DataFlow.targetType === 'resource') {
+    if (dataFlowDefinition.targetType === 'resource') {
       target = this.createResource();
     } else {
       throw 'error: only resource and webEndpoint types are allowed (webEndpoint type not implemented yet)';
     }
 
-    // TODO
+    // by default, the targetMethod has the same name as the controllerMethod
+    // if methodMap is not provided
+    const controllerMethod = dataFlowDefinition.method;
+    let targetMethod = controllerMethod;
+
+    if (dataFlowDefinition.methodMap !== undefined) {
+      if (dataFlowDefinition.methodMap[controllerMethod] === undefined) {
+        throw 'The method map provided has no corresponding target method for the given controller method'
+      }
+      targetMethod = dataFlowDefinition.methodMap[controllerMethod];
+    }
+
+    const dataFlow = {
+      controller,
+      controllerMethod,
+      target,
+      targetMethod,
+    }
 
     this.dataFlows.push(dataFlow);
   }
@@ -89,6 +117,16 @@ export default class ComponentIntegration {
     return new Controller(new DatabaseConnection());
   }
 
-  // TODO
+  integrate() {
+    if (this.dataFlows.length === 0) {
+      throw 'No dataflow has been added';
+    }
+    if (this.endpoint === undefined) {
+      throw 'An endpoint must be specified';
+    }
+    for (let i = 0; i < (this.dataFlows.length - 1); i++) {
+
+    }
+  }
 
 }
