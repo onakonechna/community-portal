d8ae3f289de96e073ed48cc2c63f6a6038dd2ab7
athensbird <markwang@utexas.edu>
2018-07-03 17:25:29 -0500

    fixed retrieveFirstname typo

--- frontend/src/components/buttons/LoginButton.tsx
+++ frontend/src/components/buttons/LoginButton.tsx
@@ -4,7 +4,7 @@ import SignInIcon from '@material-ui/icons/AccountCircle';
 import { withStyles } from '@material-ui/core/styles';
 
 const retrieveFirstName = (name: string) => {
-  return name.split(' ')[0];
+  return name.split(' ')[0] ? name.split(' ')[0] : name;
 };
 
 const styles = {
