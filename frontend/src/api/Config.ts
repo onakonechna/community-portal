fa5f08acdf06a09da9f1769ab1973c8f6909cd6f
Xiya Yang <xyang@magento.com>
2018-07-12 16:53:38 -0500

    remove hardcoded url

--- frontend/src/api/Config.ts
+++ frontend/src/api/Config.ts
@@ -1,7 +1,5 @@
-// declare const API_ENDPOINT: string;
-// export const API = API_ENDPOINT;
-// export const API = 'https://8qgklun7w3.execute-api.us-east-1.amazonaws.com/dev';
-export const API = 'http://localhost:3000';
+declare const API_ENDPOINT: string;
+export const API = API_ENDPOINT;
 
 const localToken = localStorage.getItem('oAuth');
 const token: any = localToken !== null ? JSON.parse(localToken) : '';
