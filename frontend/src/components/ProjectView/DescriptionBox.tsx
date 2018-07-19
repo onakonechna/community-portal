76ca2844acdfaded13a903f32a14ffe67c1cdd30
Xiya Yang <xyang@magento.com>
2018-07-19 11:36:29 -0500

    magento-engcom/community-portal#105 Schedule meeting for the project
    - change padding settings

--- frontend/src/components/ProjectView/DescriptionBox.tsx
+++ frontend/src/components/ProjectView/DescriptionBox.tsx
@@ -2,12 +2,9 @@ import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
-    paddingLeft: '24px',
-    paddingRight: '24px',
-    paddingTop: '6px',
-    paddingBottom: '6px',
-    fontSize: '12px',
-    backgroundColor: '#F2F3F3',
+  padding: '6px 24px',
+  fontSize: '12px',
+  backgroundColor: '#F2F3F3',
 };
 
 const DescriptionBox = (props: any) => {
