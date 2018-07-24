8cc45c7ad1e573557b114ea2f7f7504ef64078b5
Xiya Yang <xyang@magento.com>
2018-07-24 14:41:32 -0500

    fix sprint-dev errors
    - import Dispatch from redux instead of react-redux (not supported in newest version)
    - create missing OtherAction in actions

--- frontend/src/components/WithAuth.tsx
+++ frontend/src/components/WithAuth.tsx
@@ -1,6 +1,7 @@
 import * as _ from 'lodash';
 import * as React from 'react';
-import { connect, Dispatch } from 'react-redux';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import GithubAuthButton, { User }from './GithubAuthButton';
 import { onSuccess, onFailure } from './HeadBar';
 import { LoadUserAction,
