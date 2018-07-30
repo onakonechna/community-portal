d2394ef2448b876dd383ef4da45c1e0ca120eef8
Xiya Yang <xyang@magento.com>
2018-07-30 11:10:48 -0500

    magento-engcom/community-portal#211 Implement getTopUsers Handler
    - implement basic structure of getTopUsers

---
+++ functions/handlers/routine/topUsers/handler.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from './../../../config/Types';
+
+import PackageService from './../../../src/services/PackageService';
+import Endpoint from './../../../src/Endpoint';
+import {
+  ProjectController,
+  UserController,
+  ProjectResource,
+  UserResource,
+} from './../../../config/Components';
+
+const dataflows = [
+  {
+    controller: ProjectController,
+    method: 'checkPledgedHours',
+    target: ProjectResource,
+    methodMap: { checkPledgedHours: 'getById' },
+    validationMap: { checkPledgedHours: 'pledgeSchema' },
+    authDataDependencies: ['user_id'],
+  },
+  {
+    controller: ProjectController,
+    method: 'addPledgedHours',
+    target: ProjectResource,
+  },
+  {
+    controller: ProjectController,
+    method: 'addPledgedHistory',
+    target: ProjectResource,
+    dataDependencies: ['project_id', 'hours'],
+  },
+  // we want to get the avatar url
+  {
+    controller: UserController,
+    method: 'storeAvatarUrl',
+    target: UserResource,
+    methodMap: { storeAvatarUrl: 'getById' },
+    dataDependencies: ['user_id'],
+    storageSpecs: ['avatar_url'],
+  },
+  // save both user_id and avartar_url in the list of pledgers
+  {
+    controller: ProjectController,
+    method: 'addPledger',
+    target: ProjectResource,
+    dataDependencies: ['project_id', 'user_id', 'avatar_url'],
+  },
+  {
+    controller: UserController,
+    method: 'subscribe',
+    target: UserResource,
+    dataDependencies: ['user_id', 'project_id'],
+  },
+  {
+    controller: ProjectController,
+    method: 'addSubscriber',
+    target: ProjectResource,
+    dataDependencies: ['project_id', 'user_id'],
+  },
+  {
+    controller: UserController,
+    method: 'pledge',
+    target: UserResource,
+    dataDependencies: ['user_id', 'project_id', 'hours'],
+  },
+];
+
+const endpoint = new Endpoint('/user/pledge', 'post');
+endpoint.configure((req: Request, res: Response) => {
+  new PackageService(dataflows).package(req, res);
+});
+export const handler = endpoint.execute();
