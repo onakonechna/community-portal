0942baa495b434f90fa44f40778214729dbcc17e
Xiya Yang <xyang@magento.com>
2018-07-31 11:42:44 -0500

    magento-engcom/community-portal#211 Implement rankBySkill Handler
    - convert to fraction before sorting

--- functions/src/algorithms/rankBySkill.ts
+++ functions/src/algorithms/rankBySkill.ts
@@ -13,15 +13,39 @@ function increment(map: Map<string, number>, key: any) {
   map.set(key, map.get(key)+1);
 }
 
+function divide(map: Map<string, number>, key: any, divider: number) {
+  if (!divider) return 0; // divide by zero
+  map.set(key, map.get(key) / divider);
+}
+
+function divideByConstant(divider: number) {
+  return (count: number, key: string, map: Map<string, number>) => {
+    divide(map, key, divider);
+  };
+}
+
+function divideByDict(dividerDict: Map<string, number>) {
+  return (count: number, key: string, map: Map<string, number>) => {
+    divide(map, key, dividerDict.get(key));
+  };
+}
+
+function topValues(map: Map<string, number>, k: number) {
+  return [...map.entries()].sort(([k1, v1], [k2, v2]) => v2-v1).slice(0,k)
+}
+
 function rankBySkill(data: DataInterface[]) {
   let rankedUsersByProject = new Map();
   let rankedProjectsByUser = new Map();
+  let numSkillsByProject = new Map();
 
   try {
 
     for (let entry of data) {
       for (let project of entry.projects.values) {
         setIfNotExists(rankedUsersByProject, project, new Map());
+        increment(numSkillsByProject, project);
+
         for (let user of entry.users.values) {
           setIfNotExists(rankedProjectsByUser, user, new Map());
 
@@ -31,9 +55,14 @@ function rankBySkill(data: DataInterface[]) {
       }
     }
 
-    // sort and limit to top 10 users
+    // sort and limit to top 10
     for (let [project, counts] of rankedUsersByProject) {
-      rankedUsersByProject.set(project, [...counts.entries()].sort(([k1, v1], [k2, v2]) => v2-v1).slice(0,10));
+      counts.forEach(divideByConstant(numSkillsByProject.get(project)));
+      rankedUsersByProject.set(project, topValues(counts, 10));
+    }
+    for (let [user, counts] of rankedProjectsByUser) {
+      counts.forEach(divideByDict(numSkillsByProject));
+      rankedProjectsByUser.set(user, topValues(counts, 10));
     }
 
   } catch (error) {
@@ -43,4 +72,4 @@ function rankBySkill(data: DataInterface[]) {
   return { rankedUsersByProject, rankedProjectsByUser };
 }
 
-export default rankUsers;
+export default rankBySkill;
