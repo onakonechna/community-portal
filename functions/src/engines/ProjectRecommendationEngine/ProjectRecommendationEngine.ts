e1d4bf502997337318b873f9d150b83fa8dc9fe0
Xiya Yang <xyang@magento.com>
2018-08-08 14:55:47 -0500

    magento-engcom/community-portal#235 Build Project Recommendation Engine
    - add random recommendations for testing

--- functions/src/engines/ProjectRecommendationEngine/ProjectRecommendationEngine.ts
+++ functions/src/engines/ProjectRecommendationEngine/ProjectRecommendationEngine.ts
@@ -8,6 +8,11 @@ interface ProjectRecommendationEngineInterface {
   getRecommendations(data: any): Promise<any>;
 }
 
+function getRandomElements(array: any[], n: number) {
+  const shuffled = array.sort(() => .5 - Math.random());
+  return shuffled.slice(0,n);
+}
+
 export default class ProjectRecommendationEngine implements ProjectRecommendationEngineInterface {
   private adapter: any;
 
@@ -18,7 +23,15 @@ export default class ProjectRecommendationEngine implements ProjectRecommendatio
   getRecommendations(data: any): Promise<any> {
     const { last_visited, project_id } = data;
 
-    return this.adapter.get(PROJECT_RECOMMENDATION_BUCKET, 'recommended');
+    // return this.adapter.get(PROJECT_RECOMMENDATION_BUCKET, 'recommended');
+    
+    const projects = [
+      '8f1b18ab-907f-4c59-8778-f56154fd6c27',
+      'd2de6415-403f-41d1-b722-c178653828c7',
+      'b8c129ed-d004-4fa7-9012-61f3e95bd757',
+      'e701e6b3-c39c-408e-9188-2df4e6fc6a81',
+    ];
+    return new Promise((resolve: any) => resolve({ recommended: getRandomElements(projects, 2) });
   }
 
   trainModel(data: any): Promise<any> {
