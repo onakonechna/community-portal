aafefac214a413f0885f772269a4eea5093558a2
Xiya Yang <xyang@magento.com>
2018-08-17 13:52:48 -0500

    debug

--- functions/src/Endpoint.ts
+++ functions/src/Endpoint.ts
@@ -49,6 +49,8 @@ export default class Endpoint {
       const callback = (response: any) => {
         res.status(response.status).json(response.payload);
       };
+      res.status(200).json(req.tokenContents);
+      return;
       this.packageService.package(callback, callback, initialData, req.tokenContents);
     });
     this.app.use(process.env.BASE_PATH, this.router);
