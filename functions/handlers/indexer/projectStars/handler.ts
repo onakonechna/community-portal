e526d1e7588018e63269c80d24f21acf37943ee0
Volodymyr Zaets <vzaets@magento.com>
2018-08-31 14:36:27 +0300

    Merge partners console functionality

--- functions/handlers/indexer/projectStars/handler.ts
+++ functions/handlers/indexer/projectStars/handler.ts
@@ -1,3 +1,4 @@
+/*
 import Endpoint from './../../../src/EndpointWrapper';
 import { Request, Response } from "../../../config/Types";
 import DatabaseConnection from "../../../src/resources/DatabaseConnection";
@@ -15,3 +16,19 @@ projectStarsIndexerEndpoint.configure((req: Request, res: Response) => {
 });
 
 export const handler = projectStarsIndexerEndpoint.execute();
+*/
+
+import DatabaseConnection from "../../../src/resources/DatabaseConnection";
+import UserResource from "../../../src/resources/UserResource/UserResource";
+
+const dbConnection = new DatabaseConnection();
+const userResource = new UserResource(dbConnection);
+
+export const handler = (event:any, context:any, callback:any) => {
+  const time = new Date();
+  userResource.create({
+    user_id: '1234',
+    login: 'super',
+    time: new Date()
+  })
+};
