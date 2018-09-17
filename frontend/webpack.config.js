933904d23860ed255bbe9ac173269de4bc2f4afc
Volodymyr Zaets <vzaets@magento.com>
2018-09-17 14:47:01 +0300

    Add wysiwyg. Add Short Description To Project.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -95,6 +95,7 @@ module.exports = {
             },
             {
                 test:/\.css$/,
+                exclude: /Draft\.css$/,
                 use:['style-loader','css-loader']
             },
             {
