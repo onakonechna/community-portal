346bb53636dc7f4ebcedc1e61cc2613e3b65763a
Volodymyr Zaets <vzaets@magento.com>
2018-10-18 16:11:01 +0300

    Move to RDS

--- functions/webpack.config.js
+++ functions/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const externals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -19,6 +20,7 @@ module.exports = {
     filename: '[name].js',
   },
   target: 'node',
+  externals: [externals()],
   module: {
     rules: [
       { test: /\.ts(x?)$/, loader: 'ts-loader' },
