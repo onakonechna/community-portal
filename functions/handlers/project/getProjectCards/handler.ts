5b1f6f45267dc432addfac75799b21b246a022d8
Volodymyr Zaets <vzaets@magento.com>
2018-10-19 02:21:00 +0300

    Endpoint for Project Contributors

--- functions/handlers/project/getProjectCards/handler.ts
+++ functions/handlers/project/getProjectCards/handler.ts
@@ -7,7 +7,7 @@ const getProjectsEndpoint = new Endpoint('/projects/', 'get');
 console.log('START')
 
 getProjectsEndpoint.configure((req: Request, res: Response) => {
-	console.log('INSIDE', process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_USERPASSWORD, process.env.IS_OFFLINE);
+	console.log('INSIDE', process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_USERPASSWORD, process.env.DB_HOST, process.env.IS_OFFLINE);
 
 	const projectsController = new ProjectsController();
 	projectsController.execute(req, res);
