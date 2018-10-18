1050265fea6cf98170449e89e1e28df5dff375b3
Volodymyr Zaets <vzaets@magento.com>
2018-10-19 00:44:07 +0300

    Move to RDS

--- functions/handlers/project/getProjectCards/handler.ts
+++ functions/handlers/project/getProjectCards/handler.ts
@@ -4,7 +4,11 @@ import ProjectsController from '../../../src/controller/project/Projects';
 
 const getProjectsEndpoint = new Endpoint('/projects/', 'get');
 
+console.log('START')
+
 getProjectsEndpoint.configure((req: Request, res: Response) => {
+	console.log('INSIDE')
+
 	const projectsController = new ProjectsController();
 	projectsController.execute(req, res);
 });
